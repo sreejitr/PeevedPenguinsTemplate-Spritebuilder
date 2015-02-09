f3abe6b9e4cf255d79dd6771931edc6a1a16ce59
sreejitr <sreejitr@andrew.cmu.edu>
2015-02-09 14:31:06 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Penguin.m
@@ -0,0 +1,21 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by Sreejita Ray on 2/9/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Penguin created");
+    }
+    
+    return self;
+}
+@end
